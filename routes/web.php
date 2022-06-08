2acf3703ab67d46eb941cbb75b503bece402eefb
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-06-07 19:02:05 -0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//home
+Route::get('/', [HomeController::class, "index"])->name('home');
+
+//todo
+Route::prefix('/todo',)->group(function () {
+    Route::get('/', [TodoController::class, "index"])->name('todo');
+    Route::post('/store', [TodoController::class, "store"])->name('todo.store');
+    Route::get('/{task_id}/delete', [TodoController::class, "delete"])->name('todo.delete');
+    Route::get('/{task_id}/done', [TodoController::class, "done"])->name('todo.done');
+
+});
