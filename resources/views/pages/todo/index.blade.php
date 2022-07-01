25bfeaf1158da38daa2c1200d32202ea91d31d93
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-06-30 23:59:40 -0700

    a

--- resources/views/pages/todo/index.blade.php
+++ resources/views/pages/todo/index.blade.php
@@ -68,7 +68,7 @@
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              ...
+              <h1>modal</h1>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
