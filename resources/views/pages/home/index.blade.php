e5aba3ffe1f5cd7380916806612851b940325341
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-07-02 22:22:43 -0700

    a

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -11,13 +11,13 @@
             @forelse ($banners as banner)
             <div class="col lg-4">
                 <div class="card">
-                    {{-- <img src = "{{ config('images.access_path') }}/{{ $banner->images->name }}" class="banner-image" alt="banner image"
+                    <img src = "{{ config('images.access_path') }}/{{ $banner->images->name }}" class="banner-image" alt="banner image"
                     class="banner-image-card-img-top">
                     <div class="card-body">
                       <h5 class="card-title">{{ $banner->title }}</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                       <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div> --}}
+                    </div>
                   </div>
             </div>
             @empty
