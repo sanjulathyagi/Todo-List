4b70145e1c97206244ccf6982fdf1412f6beacb0
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-07-14 23:50:51 -0700

    a

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -15,8 +15,8 @@
                     class="banner-image-card-img-top">
                     <div class="card-body">
                       <h5 class="card-title">{{ $banner->title }}</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a> --}}
                     </div>
                   </div>
             </div>
