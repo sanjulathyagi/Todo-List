63d14ae16836fc6bd2b177dde5b365de8f753f6e
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-07-24 23:15:12 -0700

    a

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array = [
+            'name' => 'Category 1',
+            'name' => 'Category 1',
+            'name' => 'Category 2',
+            'name' => 'Category 3',
+            'name' => 'Category 4',
+            'name' => 'Category 5',
+            'name' => 'Category 6',
+            'name' => 'Category 7',
+            'name' => 'Category 8',
+            'name' => 'Category 9',
+            'name' => 'Category 10',
+
+        ];
+        foreach ($array as $value){
+            \App\Models\Category::create([
+                'name' => $value,
+            ]);
+        }
+    }
+}
